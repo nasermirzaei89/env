326be115935e4bd5fa959491da3e7df572d1c761
Naser Mirzaei <nasermirzaei89@gmail.com>
2019-10-12 16:25:29 +0330

    use Run for subtests of bool

--- bool_test.go
+++ bool_test.go
@@ -8,52 +8,81 @@ import (
 )
 
 func TestGetBool(t *testing.T) {
-	os.Clearenv()
+	t.Run("GetAbsentBoolWithDefault", func(t *testing.T) {
+		os.Clearenv()
+		res := env.GetBool("V1", true)
+		assert.True(t, res)
+	})
 
-	res := env.GetBool("V1", true)
-	assert.True(t, res)
+	t.Run("GetPresentBoolWithDefault", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "false")
+		res := env.GetBool("V1", true)
+		assert.False(t, res)
+	})
 
-	_ = os.Setenv("V1", "false")
-	res = env.GetBool("V1", true)
-	assert.False(t, res)
+	t.Run("GetZeroAsBoolWithDefault", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "0")
+		res := env.GetBool("V1", true)
+		assert.False(t, res)
+	})
 
-	_ = os.Setenv("V1", "0")
-	res = env.GetBool("V1", true)
-	assert.False(t, res)
+	t.Run("GetTrueStringAsBoolWithDefault", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "true")
+		res := env.GetBool("V1", false)
+		assert.True(t, res)
+	})
 
-	_ = os.Setenv("V1", "true")
-	res = env.GetBool("V1", false)
-	assert.True(t, res)
+	t.Run("GetEmptyAsBoolWithDefault", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "")
+		res := env.GetBool("V1", false)
+		assert.True(t, res)
+	})
 
-	_ = os.Setenv("V1", "")
-	res = env.GetBool("V1", false)
-	assert.True(t, res)
-
-	_ = os.Setenv("V1", "1")
-	res = env.GetBool("V1", false)
-	assert.True(t, res)
+	t.Run("GetOneAsBoolWithDefault", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "1")
+		res := env.GetBool("V1", false)
+		assert.True(t, res)
+	})
 }
 
 func TestMustGetBool(t *testing.T) {
-	os.Clearenv()
-
-	assert.Panics(t, func() {
-		env.MustGetBool("V1")
+	t.Run("MustGetAbsentBool", func(t *testing.T) {
+		os.Clearenv()
+		assert.Panics(t, func() {
+			env.MustGetBool("V1")
+		})
 	})
 
-	_ = os.Setenv("V1", "false")
-	res := env.MustGetBool("V1")
-	assert.False(t, res)
+	t.Run("MustGetFalseStringAsBool", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "false")
+		res := env.MustGetBool("V1")
+		assert.False(t, res)
+	})
 
-	_ = os.Setenv("V1", "0")
-	res = env.MustGetBool("V1")
-	assert.False(t, res)
+	t.Run("MustGetZeroAsBool", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "0")
+		res := env.MustGetBool("V1")
+		assert.False(t, res)
+	})
 
-	_ = os.Setenv("V1", "true")
-	res = env.MustGetBool("V1")
-	assert.True(t, res)
+	t.Run("MustGetTrueStringAsBool", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "true")
+		res := env.MustGetBool("V1")
+		assert.True(t, res)
+	})
 
-	_ = os.Setenv("V1", "1")
-	res = env.MustGetBool("V1")
-	assert.True(t, res)
+	t.Run("MustGetOneAsBool", func(t *testing.T) {
+		os.Clearenv()
+		_ = os.Setenv("V1", "1")
+		res := env.MustGetBool("V1")
+		assert.True(t, res)
+	})
 }
