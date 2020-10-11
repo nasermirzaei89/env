63f2ffcfa76593693b40fe15e5d1c40b6cf800a0
Naser Mirzaei <nasermirzaei89@gmail.com>
2020-10-12 00:06:38 +0330

    add string slice
    Signed-off-by: Naser Mirzaei <nasermirzaei89@gmail.com>

---
+++ string_slice.go
@@ -0,0 +1,37 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// GetStringSlice extracts slice of strings value with format "foo,bar,baz" from env
+// if not set, returns default value
+func GetStringSlice(key string, def []string) []string {
+	s, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+
+	if len(s) == 0 {
+		return []string{}
+	}
+
+	return strings.Split(s, ",")
+}
+
+// MustGetString extracts slice of strings value with format "foo,bar,baz" from env
+// if not set, it panics
+func MustGetStringSlice(key string) []string {
+	s, ok := os.LookupEnv(key)
+	if !ok {
+		panic(fmt.Sprintf("environment variable '%s' has not been set", key))
+	}
+
+	if len(s) == 0 {
+		return []string{}
+	}
+
+	return strings.Split(s, ",")
+}
